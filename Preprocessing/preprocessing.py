0f5ae85c986ce4092008014fee04106aa1e648aa
Parzival <wen19990116@gmail.com>
2021-01-13 00:35:45 +0800

    Code from notebook to .py

--- Preprocessing/preprocessing.py
+++ Preprocessing/preprocessing.py
@@ -2,29 +2,51 @@ import pandas as pd
 import numpy as np
 import pickle
 from collections import Counter
-path = 'dataset/'
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+path = '../dataset/'
 
 doc_df = pd.read_csv(path + 'documents.csv')
 doc_dict = dict()
 # Fill nan(float) as none(str)
 doc_df = doc_df.fillna('')
+Languages = list(stopwords.fileids())
 
 # Build remove terms
-remove_title = ['[Text]', 'Language: <F P=105>', '</F>', 'Article Type:BFN', '<F P=106>', 'Article Type:CSO', '[Excerpt]', '[Editorial Report]', '[passage omitted]']
-remove_term = ['.', '"', '--', '\'s', '<', '>', '[', ']', '`', ',', ':', '/', '\\', '{', '}']
+remove_title = ['[Text]', 'Language:', '<F P=105>', '</F>', 'Article Type:BFN', '<F P=106>', 'Article Type:CSO', '[Excerpt]', '[Editorial Report]', '[passage omitted]', 'ONLY <F P=103>', '<F P=104>']
+remove_term = ['.', '"', '--', '\'s', '<', '>', '[', ']', '`', ',', ':', '/', '\\', '{', '}', '-', '(', ')']
 # Build dictionary
 for doc in doc_df.iloc:
     temp_str = doc['doc_text']
+    # Choosing the fileids of stopwords, initial:english
+    Lang_tmp = 'english'
+    Lang_flag = False
+    if('Language: <F P=105>' in temp_str):
+        Lang_flag = True
+        Lang_tmp = temp_str.split('Language: <F P=105>')[1].split()[0]
+        if(not(Lang_tmp.lower() in Languages)):
+            Lang_flag = False
+    # Removing meaningless words
     for t in remove_title:
         if(t in temp_str):
             temp_str = temp_str.replace(t, '')
     for w in remove_term:
         if(w in temp_str):
             temp_str = temp_str.replace(w, '')
+    # Removing stopwords
     temp = Counter(temp_str.split())
+    if(Lang_flag):
+        # stopwords with origin and capitalize form
+        for s in stopwords.words(Lang_tmp.lower()):
+            if(s in temp):
+                del temp[s]
+            if(s.capitalize() in temp):
+                del temp[s.capitalize()]
+    # Save to dict    
     doc_dict[doc['doc_id']] = temp
 
-    # File output
-pickle_out = open('./data/document.pickle', 'wb')
+# File output
+pickle_out = open('../data/document.pickle', 'wb')
 pickle.dump(doc_dict, pickle_out)
 pickle_out.close()
