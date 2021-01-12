d8de8a79757f5a23da80b899a1e736f830da81e0
Danny <dannylee94049@gmail.com>
2021-01-12 18:02:55 +0800

    update bm25
    add argument parser and scratch

--- BM25/bm25.py
+++ BM25/bm25.py
@@ -5,7 +5,14 @@ from scipy.sparse import csr_matrix
 import numpy as np
 from numba import njit
 
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-doc_path", type=str, required=True, help="document dictionary file path (.pkl)")
+parser.add_argument("-result_path", type=str, required=True)
+parser.add_argument("-hidden_size", type=int, default=50)
+parser.add_argument("-scratch", type=int, default=0)
+args = parser.parse_args()
 
 def term_frequency(index_term_dict, document):
     data = []
@@ -44,55 +51,62 @@ def _term_bm25_weight(tf_data, tf_row, tf_col, len_term, idf_data, bm_doc_len_da
     return term_bm25_weight
 
 
+if args.scratch:
+    # Reading document pickle file
+    print('Reading document pickle file')
+    with open(args.doc_path, 'rb') as f:
+        document = pickle.load(f)
 
-# Reading document pickle file
-print('Reading document pickle file')
-with open('./data/document.pickle', 'rb') as f:
-    document = pickle.load(f)
+    # Get index_term
+    index_term = set()
+    for doc in tqdm(document.keys(), desc='Get index_term'):
+        index_term |= set(document[doc].keys())
+    index_term = list(index_term)
 
-# Get index_term
-index_term = set()
-for doc in tqdm(document.keys(), desc='Get index_term'):
-    index_term |= set(document[doc].keys())
-index_term = list(index_term)
+    with open("data/index_term.pkl", 'wb') as f:
+        pickle.dump(index_term, f)
 
-# k:term , v:term_index
-index_term_dict = {k: v for v, k in enumerate(index_term)}
+    # k:term , v:term_index
+    index_term_dict = {k: v for v, k in enumerate(index_term)}
 
-# k:doc_id , v:doc_index
-doc_dict = {k: v for v, k in enumerate(list(document.keys()))}
+    # k:doc_id , v:doc_index
+    doc_dict = {k: v for v, k in enumerate(list(document.keys()))}
 
-# TF, DF, IDF
-tf_matrix = term_frequency(index_term_dict, document)
-df_matrix = document_frequency(tf_matrix)
-idf_matrix = np.log((len(document) - df_matrix + 0.5) / (df_matrix + 0.5))
+    # TF, DF, IDF
+    tf_matrix = term_frequency(index_term_dict, document)
+    df_matrix = document_frequency(tf_matrix)
+    idf_matrix = np.log((len(document) - df_matrix + 0.5) / (df_matrix + 0.5))
 
-# BM25
-print('Create BM25 matrix')
-k1 = 2.5
-b = 0.8
-doc_len = [sum(document[doc].values()) for doc in document.keys()]
-avg_doclen = sum(doc_len) / len(document)
-tf_matrix_T = tf_matrix.tocoo().transpose()
+    # BM25
+    print('Create BM25 matrix')
+    k1 = 2.5
+    b = 0.8
+    doc_len = [sum(document[doc].values()) for doc in document.keys()]
+    avg_doclen = sum(doc_len) / len(document)
+    tf_matrix_T = tf_matrix.tocoo().transpose()
 
-bm25_matrix = np.zeros(len(index_term))
-v2 = np.array(doc_len)
-v2 = k1 * ((1 - b) * b * (v2 / avg_doclen))
-bm25_matrix = _term_bm25_weight(tf_matrix_T.data, tf_matrix_T.row, tf_matrix_T.col, len(index_term_dict), idf_matrix, v2, k1)
+    bm25_matrix = np.zeros(len(index_term))
+    v2 = np.array(doc_len)
+    v2 = k1 * ((1 - b) * b * (v2 / avg_doclen))
+    bm25_matrix = _term_bm25_weight(tf_matrix_T.data, tf_matrix_T.row, tf_matrix_T.col, len(index_term_dict), idf_matrix, v2, k1)
 
-# Save BM25 matrix
-print('Save BM25 matrix')
-np.save('bm25_matrix.npy', bm25_matrix)
+    # Save BM25 matrix
+    print('Save BM25 matrix')
+    np.save('data/bm25_matrix.npy', bm25_matrix)
+else:
+    with open("data/index_term.pkl", 'rb') as f:
+        index_term = pickle.load(f)
+    bm25_matrix = np.load('data/bm25_matrix.npy')
 
 # Get feature_words
 print('Get feature_words')
-feature_amount = 50
+feature_amount = args.hidden_size
 feature_words = []
 for term_index in np.flip(bm25_matrix.argsort())[:feature_amount]:
     feature_words.append(index_term[term_index])
 
 print('Save feature_words')
-with open('./data/feature_words.pickle', 'wb') as f:
+with open(args.result_path, 'wb') as f:
     pickle.dump(feature_words, f)
 
 print('Done')
